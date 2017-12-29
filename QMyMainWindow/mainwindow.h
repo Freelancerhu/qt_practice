fc87f3f796c64218ce3052682a67fa3a88afa9cb
freelancerhu <freelancerhu01@gmail.com>
2017-12-29 17:02:58 -0500

    update mainwindow with several funtions.

--- QMyMainWindow/mainwindow.h
+++ QMyMainWindow/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCloseEvent>
 
 namespace Ui {
 class MainWindow;
@@ -11,12 +12,47 @@ class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
+protected:
+  // close event
+  void closeEvent(QCloseEvent *event);
+
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
+  void newFile();
+  bool maybeSave();
+  bool save();
+  bool saveAs();
+  bool saveFile (const QString &fileName);
+  bool loadFile(const QString &fileName);
+
+private slots:
+  void on_actionNew_File_N_triggered();
+
+  void on_actionSave_S_triggered();
+
+  void on_actionSaveAS_A_triggered();
+
+  void on_actionOpen_File_O_triggered();
+
+  void on_actionExit_X_triggered();
+
+  void on_actionUndo_Z_triggered();
+
+  void on_actionCut_C_triggered();
+
+  void on_actionCopy_C_triggered();
+
+  void on_actionPaste_V_triggered();
+
+  void on_actionClose_File_C_triggered();
 
 private:
   Ui::MainWindow *ui;
+  // whether the file is saved
+  bool isUntitled;
+  // path of the file
+  QString curFile;
 };
 
 #endif // MAINWINDOW_H
