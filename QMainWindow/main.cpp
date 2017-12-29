ad61d31706c4b42f7e2242a5166419daa0ec97dd
freelancerhu <freelancerhu01@gmail.com>
2017-12-29 13:41:32 -0500

    QMainWindow

---
+++ QMainWindow/main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QTextCodec>
+#include "logindlg.h"
+
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  MainWindow w;
+  loginDlg dlg;
+  if (dlg.exec() == QDialog::Accepted) {
+    w.show();
+    return a.exec();
+  }
+  else return 0;
+}
